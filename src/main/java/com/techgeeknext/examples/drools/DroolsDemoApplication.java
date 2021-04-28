d50f877bf1b765f4f9219c3058303066a03a6afc
TechGeekNeXt <techgeeknext@gmail.com>
2021-04-28 16:09:16 +0530

    https://www.techgeeknext.com/spring-boot/drools-example

---
+++ src/main/java/com/techgeeknext/examples/drools/DroolsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.techgeeknext.examples.drools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroolsDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DroolsDemoApplication.class, args);
+    }
+
+}
